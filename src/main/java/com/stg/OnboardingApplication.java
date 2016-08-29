639fffbe9a06339bef08b05622cce59cf69fde1b
David <david@stgconsulting.com>
2016-08-29 14:10:34 -0600

    Preparing for OAuth2

--- src/main/java/com/stg/OnboardingApplication.java
+++ src/main/java/com/stg/OnboardingApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {
-	org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = { org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class })
 @EnableScheduling
+// @EnableOAuth2Sso
 public class OnboardingApplication {
 
     public static void main(String[] args) {
